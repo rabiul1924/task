bf8bb1e23a5efe3a5351e83ec6c4126211e00ace
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-06-27 11:29:35 +0600

    navbar create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar.js";
+
+
 
 function App() {
   return (
-    <div className="bg-red-200">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+      <Navbar />
+      <Switch>
+        {/* <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/addAdmin">
+          <AddAdmin />
+        </Route>
+        <Route path="/addBlog">
+          <AddBlog />
+        </Route>
+        <Route path="/manageBlog">
+          <ManageBlog />
+        </Route>
+        <Route path="/blog/:id">
+          <SingleBlog />
+        </Route> */}
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+const Navbar = () => {
+    const [showDropdown, setShowDropDown] = useState(false)
+    return (
+        <nav className="md:col-span-1 md:flex md:justify-between fixed w-full px-4 bg-white shadow-sm py-4">
+            <div className="flex justify-between items-center">
+                <NavLink to="/">
+                    TAsk
+                </NavLink>
+                <div className="px-4 cursor-pointer md:hidden" onClick={() => setShowDropDown(!showDropdown)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </div>
+            </div>
+            <div className={`my-2 ${showDropdown ? 'flex flex-col' : 'hidden'} md:block`}>
+                <NavLink
+                    to="/"
+                    exact
+                    activeClassName="border-b-2 border-grash"
+                    className="mx-4 py-2 uppercase inline-block text-xs font-bold hover:border-b-2 hover:border-grash"
+                >
+                    Home
+                </NavLink>
+               
+            </div>
+        </nav >
+    );
+};
+
+export default Navbar;
