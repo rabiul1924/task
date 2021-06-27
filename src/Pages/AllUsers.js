717bb7272ff787265e113d0a91a52c17fc3f8724
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-06-28 01:15:48 +0600

    columns

--- src/Pages/AllUsers.js
+++ src/Pages/AllUsers.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AllUsers = () => {
+    const[ users, setUsers] = useState([])
+    const[sortType, setSortType] = useState('asc')
+    // const[sortDsc, setSortDscS] = useState('dsc')
+
+
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data =>  setUsers(data))
+    },[])
+
+    const handleTextChange = e => {
+       const filterUser = users.filter(user => user.name.toLowerCase().includes(e.target.value.toLowerCase()))
+       setUsers(filterUser);
+    }
+    const handleEmailChange = e => {
+       const filterUser = users.filter(user => user.email.toLowerCase().includes(e.target.value.toLowerCase()))
+       setUsers(filterUser);
+    }
+    const handleWebsiteChange = e => {
+       const filterUser = users.filter(user => user.website.toLowerCase().includes(e.target.value.toLowerCase()))
+       setUsers(filterUser);
+    }
+
+    const handleSorted = () =>{
+         setUsers( users.sort((a,b) => {
+            const isReverse = (sortType === "asc") ? 1 : -1 
+            return isReverse * a.name.localeCompare(b.name) 
+         }))
+        //  setSortType(sorted)
+        //  console.log(sorted)
+        // setUsers(sorted)
+
+    }
+    console.log(users)
+    
+    // const handleDscSort = () => {
+    //     const sorted = users.sort((a,b) => {
+    //         const isReverse = (sortType === "dsc") ? 1 : -1 
+    //         return isReverse * a.name.localeCompare(b.name) 
+    //      })
+    //      setSortType(sorted)
+    // }
+    
     return (
-        <div>
+        <div className="pt-24 text-center">
             <h1>All Users</h1>
+            <button className="btn" onClick={handleSorted}>ASC</button>
+            {/* <button className="btn" onClick={handleDscSort}>DSC</button> */}
+            <div className="flex justify-between px-12 py-4 bg-gray-400 text-white">
+                <h1>Name</h1>
+                <h1>Email</h1>
+                <h1>Website</h1>
+            </div>
+            <div className="flex justify-between px-12 py-4">
+                <input onChange={handleTextChange} type="text" placeholder="Name" />
+                <input onChange={handleEmailChange} type="text" placeholder="Gmail" />
+                <input onChange={handleWebsiteChange} type="text" placeholder="Website" />
+            </div>
+            <div>
+                {users.map(user => 
+                   <div className="flex justify-between px-12 py-4"> 
+                        <h1>{user.name}</h1>
+                        <h1>{user.email}</h1>
+                        <h1>{user.website}</h1>
+                   </div> )}
+            </div>
         </div>
     );
 };
--- src/Pages/SinglePostDetails.js
+++ src/Pages/SinglePostDetails.js
@@ -3,14 +3,25 @@ import { useParams } from 'react-router-dom';
 
 const SinglePostDetails = () => {
     const [comments, setComments] = useState([])
+    const [post, setPost] = useState({})
     const {id} = useParams();
     useEffect(()=>{
-        fetch('http://jsonplaceholder.typicode.com/posts/1/comments')
+        fetch(`http://jsonplaceholder.typicode.com/posts/${id}/comments`)
         .then(res => res.json())
         .then(data => setComments(data))
     },[id])
+    useEffect(()=>{
+        fetch(`http://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => res.json())
+        .then(data => setPost(data))
+    },[id])
     return (
         <div className="pt-24">
+            <div>
+                <h4>Post ID: {post.id}</h4>
+                <h4>Title: {post.title}</h4>
+                <h4>Body: {post.body}</h4>
+            </div>
             <header className="text-center">All Comments</header>
            <div className="grid grid-cols-2">
            {comments.map(comment => (    
