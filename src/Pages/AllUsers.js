c8cc3c4ee4a61e956bf6592ce2070f759de23c9f
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-06-28 12:19:20 +0600

    asc and dsc complete

--- src/Pages/AllUsers.js
+++ src/Pages/AllUsers.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 
 const AllUsers = () => {
     const[ users, setUsers] = useState([])
-    const[sortType, setSortType] = useState('asc')
-    // const[sortDsc, setSortDscS] = useState('dsc')
+    const [sort, setSort] = useState('asc');
+    const[currentPage, setCurrentPage] = useState(1);
+    const[postsPerPage, setPostsPerPage] = useState(5);
+    const pageNumbers = [];
 
+    for (let i =1; i <= Math.ceil(users.length / postsPerPage); i++){
+        pageNumbers.push(i)
+    }
 
+    
 
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,31 +32,24 @@ const AllUsers = () => {
        setUsers(filterUser);
     }
 
-    const handleSorted = () =>{
-         setUsers( users.sort((a,b) => {
-            const isReverse = (sortType === "asc") ? 1 : -1 
-            return isReverse * a.name.localeCompare(b.name) 
-         }))
-        //  setSortType(sorted)
-        //  console.log(sorted)
-        // setUsers(sorted)
-
+    const handleSorted = (sortType) => {
+        const sorted = users.sort((a, b) => {
+            const isReverse = (sortType === "asc") ? 1 : -1
+            return isReverse * a.name.localeCompare(b.name)
+        })
+        setSort(sortType)
+        setUsers(sorted)
     }
-    console.log(users)
-    
-    // const handleDscSort = () => {
-    //     const sorted = users.sort((a,b) => {
-    //         const isReverse = (sortType === "dsc") ? 1 : -1 
-    //         return isReverse * a.name.localeCompare(b.name) 
-    //      })
-    //      setSortType(sorted)
-    // }
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage; 
+    const currentPosts = users.slice(indexOfFirstPost, indexOfLastPost);
     
     return (
         <div className="pt-24 text-center">
             <h1>All Users</h1>
-            <button className="btn" onClick={handleSorted}>ASC</button>
-            {/* <button className="btn" onClick={handleDscSort}>DSC</button> */}
+            <button className="btn" onClick={() => handleSorted('asc')}>ASC</button>
+            <button className="btn" onClick={() => handleSorted('dsc')}>DSC</button>
             <div className="flex justify-between px-12 py-4 bg-gray-400 text-white">
                 <h1>Name</h1>
                 <h1>Email</h1>
@@ -62,12 +61,17 @@ const AllUsers = () => {
                 <input onChange={handleWebsiteChange} type="text" placeholder="Website" />
             </div>
             <div>
-                {users.map(user => 
+                {currentPosts.map(user => 
                    <div className="flex justify-between px-12 py-4"> 
                         <h1>{user.name}</h1>
                         <h1>{user.email}</h1>
                         <h1>{user.website}</h1>
                    </div> )}
+            </div>
+            <div>
+                {pageNumbers.map(pageNumber => (
+                    <button>{pageNumber}</button>
+                ))}
             </div>
         </div>
     );
